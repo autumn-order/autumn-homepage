f49cfdc4871f09dab5a4264bb2dca5a14ff21e67
Hyziri <hyziri@protonmail.com>
2024-12-01 20:59:19 -0600

    Update website links on footer & update social media links

--- src/web/components/footer.rs
+++ src/web/components/footer.rs
@@ -1,17 +1,61 @@
 use dioxus::prelude::*;
-use dioxus_free_icons::icons::fa_brands_icons::{FaDiscord, FaGithub};
+use dioxus_free_icons::icons::fa_brands_icons::{
+    FaBluesky, FaDiscord, FaGithub, FaReddit, FaSoundcloud, FaTwitch, FaYoutube,
+};
 use dioxus_free_icons::Icon;
 
-use crate::web::constant::{DISCORD_URL, EVE_LEGAL_STATEMENT, GITHUB_URL};
+use crate::web::constant::{
+    BLUESKY_URL, DISCORD_URL, GITHUB_URL, REDDIT_URL, SOUNDCLOUD_URL, TWITCH_URL, YOUTUBE_URL,
+};
+
+pub struct FooterSection {
+    pub title: &'static str,
+    pub links: Vec<FooterLink>,
+}
+
+#[derive(PartialEq, Clone, Props)]
+pub struct FooterLink {
+    pub text: &'static str,
+    pub href: &'static str,
+}
 
 #[component]
 pub fn Footer() -> Element {
     const AUTUMN_LOGO: Asset = asset!("/assets/autumn-logo-dark.png");
 
+    let footer_sections: Vec<FooterSection> = vec![
+        FooterSection {
+            title: "Services",
+            links: vec![
+                FooterLink {
+                    text: "Apply",
+                    href: "https://apply.autumn-order.com",
+                },
+                FooterLink {
+                    text: "SeAT",
+                    href: "https://seat.autumn-order.com",
+                },
+            ],
+        },
+        FooterSection {
+            title: "Information",
+            links: vec![
+                FooterLink {
+                    text: "Wiki",
+                    href: "https://wiki.autumn-order.com",
+                },
+                FooterLink {
+                    text: "Roadmap",
+                    href: "https://trello.com/b/2kdvKXnu/autumn-roadmap",
+                },
+            ],
+        },
+    ];
+
     rsx! {
-        footer { class: "footer footer-center bg-base-200 text-base-content p-6 md:p-10 justify-center",
-            div { class: "max-w-[1440px] w-full flex justify-between flex-wrap",
-                aside {
+        div { class: "flex flex-col items-center w-full py-10 bg-base-200",
+            footer { class: "footer text-base-content px-4 max-w-[1440px] w-full flex gap-12 flex-wrap",
+                aside { class: "w-full sm:w-fit",
                     a {
                         href: "/",
                         class: "btn btn-ghost flex items-center gap-2",
@@ -23,29 +67,61 @@ pub fn Footer() -> Element {
                         p { class: "text-2xl font-bold", "Autumn" }
                     }
                 }
-                nav { class: "flex gap-2 w-full justify-between sm:w-fit sm:justify-end",
-                    ul { class: "flex list-none",
-                        li {
-                            a {
-                                class: "btn btn-square btn-ghost",
-                                href: DISCORD_URL,
-                                aria_label: "Discord",
-                                Icon { width: 24, height: 24, icon: FaDiscord }
-                            }
-                        }
-                        li {
-                            a {
-                                class: "btn btn-square btn-ghost",
-                                href: GITHUB_URL,
-                                aria_label: "GitHub",
-                                Icon { width: 24, height: 24, icon: FaGithub }
-                            }
+                for (key, value) in footer_sections.iter().enumerate() {
+                    nav { key: "{key}",
+                        h6 { class: "footer-title", "{value.title}" }
+                        for (key, value) in value.links.iter().enumerate() {
+                            a { key: "{key}", class: "link link-hover", href: value.href, "{value.text}" }
                         }
                     }
-                    a { href: "/join-autumn", class: "btn px-2 md:px-4 btn-primary", "Join Autumn" }
                 }
             }
-            p { class: "text-xs", {EVE_LEGAL_STATEMENT}}
+            footer { class: "footer text-base-content px-4 pt-8 max-w-[1440px] w-full",
+                ul { class: "flex flex-wrap gap-4",
+                    li {
+                        a { href: DISCORD_URL, class: "footer-media-link hover:bg-[#5865F2]",
+                            Icon { width: 24, height: 24, icon: FaDiscord }
+
+                        }
+                    }
+                    li {
+                        a { href: YOUTUBE_URL, class: "footer-media-link hover:bg-[#FF0000]",
+                            Icon { width: 24, height: 24, icon: FaYoutube }
+
+                        }
+                    }
+                    li {
+                        a { href: TWITCH_URL, class: "footer-media-link hover:bg-[#9146FF]",
+                            Icon { width: 24, height: 24, icon: FaTwitch }
+
+                        }
+                    }
+                    li {
+                        a { href: BLUESKY_URL, class: "footer-media-link hover:bg-[#1D9BF0]",
+                            Icon { width: 24, height: 24, icon: FaBluesky }
+
+                        }
+                    }
+                    li {
+                        a { href: REDDIT_URL, class: "footer-media-link hover:bg-[#FF4500]",
+                            Icon { width: 24, height: 24, icon: FaReddit }
+
+                        }
+                    }
+                    li {
+                        a { href: GITHUB_URL, class: "footer-media-link hover:bg-[#000000]",
+                            Icon { width: 24, height: 24, icon: FaGithub }
+
+                        }
+                    }
+                    li {
+                        a { href: SOUNDCLOUD_URL, class: "footer-media-link hover:bg-[#F26F23]",
+                            Icon { width: 24, height: 24, icon: FaSoundcloud }
+
+                        }
+                    }
+                }
+            }
         }
     }
 }
